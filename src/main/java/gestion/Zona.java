6320bd71942e11d4f9f887cf00e23e03081a9e1a
Savarelav18 <111089079+Savarelav18@users.noreply.github.com>
2022-09-25 14:56:15 -0500

    inicializacion de array list
    se crea un nuevo objeto arraylist de todos los array presentes en el proyecto

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 
 public class Zona {
     private String nombre;
-    private ArrayList<Zoologico> zoo;
-    private ArrayList<Animal> animales;
+    private ArrayList<Zoologico> zoo = new ArrayList<>();
+    private ArrayList<Animal> animales = new ArrayList<>();
 
     public Zona(String nombre,ArrayList<Zoologico>zoo,ArrayList<Animal>animales){
         this.nombre=nombre;
